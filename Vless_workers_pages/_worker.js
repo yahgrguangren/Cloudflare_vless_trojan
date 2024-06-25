fe468878f31047452d4e59d54b84a7f7035dffc1
yahgr <55775770+yahgrguangren@users.noreply.github.com>
2024-06-25 21:29:51 +0800

    Update _worker.js

--- Vless_workers_pages/_worker.js
+++ Vless_workers_pages/_worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "77a571fb-4fd2-4b37-8596-1b7d9728bb5c";
+let userID = "c0cf11e0-827f-4e84-83fa-8d5f44b79371";
 
 const proxyIPs = ["cdn.xn--b6gac.eu.org"]; //workers.cloudflare.cyou bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org
 const cn_hostnames = [''];
